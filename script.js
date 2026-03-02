d8dfd9f075561d837e0209373b0ba05c4bdb9630
Flavio Luis <46407045+flavioluis9@users.noreply.github.com>
2026-03-02 17:12:11 +0400

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+// Get computer choice function
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    let computerChoice = "";
+
+    if (randomNumber < (1/3)) {
+        computerChoice = "Rock";
+    } else if (randomNumber > (2/3)) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+    return computerChoice;
+}
+
+    console.log(getComputerChoice());
